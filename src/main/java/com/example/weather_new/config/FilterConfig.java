e20f91a2df6aa098c862baaf80895e6913eec5b9
Ваше Имя <_email@example.com>
2026-01-30 09:53:46 +0300

    prepare to deploy

--- src/main/java/com/example/weather_new/config/FilterConfig.java
+++ src/main/java/com/example/weather_new/config/FilterConfig.java
@@ -1,19 +1,19 @@
-//package com.example.weather_new.config;
-//
-//
-//import com.example.weather_new.service.AuthServiceFilter.AuthFilter;
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class FilterConfig {
-//
-//    @Bean
-//    public FilterRegistrationBean<AuthFilter> authFilter() {
-//        FilterRegistrationBean<AuthFilter> registration = new FilterRegistrationBean<>();
-//        registration.setFilter(new AuthFilter());
-//        registration.addUrlPatterns("/*");
-//        return registration;
-//    }
-//}
+package com.example.weather_new.config;
+
+
+import com.example.weather_new.service.AuthServiceFilter.AuthFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<AuthFilter> authFilter() {
+        FilterRegistrationBean<AuthFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new AuthFilter());
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
+}
